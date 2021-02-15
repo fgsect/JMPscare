19d41f55fd28648620b472228633fb432b4663a1
Dominik Maier <domenukk@gmail.com>
2021-02-15 23:45:49 +0100

    code format

--- collection/python/jmpscare_col/__init__.py
+++ collection/python/jmpscare_col/__init__.py
@@ -5,14 +5,14 @@ import zlib
 def setup_cov_collection(uc, code_start, code_end, cov_data):
     def callback(uc, address, size, user_data=None):
         cov_data.append(address)
-    
+
     uc.hook_add(UC_HOOK_CODE, callback, None, code_start, code_end)
 
 
 def write_cov_data(trace):
     h = zlib.adler32(str(trace).encode())
-    with open("./cov/" + str(h), 'w+') as f:
+    with open("./cov/" + str(h), "w+") as f:
         for addr in trace:
-            f.write(hex(addr) + '\n')
+            f.write(hex(addr) + "\n")
 
     return True
--- plugins/binaryninja/jmpscare/__init__.py
+++ plugins/binaryninja/jmpscare/__init__.py
@@ -1,16 +1,23 @@
 from binaryninja import *
-from binaryninjaui import DockHandler, DockContextHandler, UIActionHandler, getMonospaceFont
+from binaryninjaui import (
+    DockHandler,
+    DockContextHandler,
+    UIActionHandler,
+    getMonospaceFont,
+)
 from PySide2 import QtCore
 from PySide2.QtCore import Qt, QAbstractTableModel
 from PySide2.QtWidgets import *
-from PySide2.QtGui import (QFont, QFontMetricsF, QTextCursor, QBrush)
+from PySide2.QtGui import QFont, QFontMetricsF, QTextCursor, QBrush
 
-colors = {"black":   HighlightStandardColor.BlackHighlightColor,
-          "cyan":    HighlightStandardColor.CyanHighlightColor,
-          "green":   HighlightStandardColor.GreenHighlightColor,
-          "magenta": HighlightStandardColor.MagentaHighlightColor,
-          "orange":  HighlightStandardColor.OrangeHighlightColor,
-          "red":     HighlightStandardColor.RedHighlightColor}
+colors = {
+    "black": HighlightStandardColor.BlackHighlightColor,
+    "cyan": HighlightStandardColor.CyanHighlightColor,
+    "green": HighlightStandardColor.GreenHighlightColor,
+    "magenta": HighlightStandardColor.MagentaHighlightColor,
+    "orange": HighlightStandardColor.OrangeHighlightColor,
+    "red": HighlightStandardColor.RedHighlightColor,
+}
 
 
 class TableModel(QAbstractTableModel):
@@ -18,16 +25,10 @@ class TableModel(QAbstractTableModel):
         super(TableModel, self).__init__()
         self._data = data
         self._patched = []
-        self.COLUMN_HEADERS = [
-            'Address',
-            'Condition',
-            'Taken',
-            'New Cov'
-        ]
+        self.COLUMN_HEADERS = ["Address", "Condition", "Taken", "New Cov"]
 
         self._last_sort = 0
         self._last_sort_order = QtCore.Qt.AscendingOrder
-
 
     def data(self, index, role=QtCore.Qt.DisplayRole):
         if role == QtCore.Qt.DisplayRole:
@@ -42,21 +43,20 @@ class TableModel(QAbstractTableModel):
             addr = int(self._data[index.row()][0], 16)
             if addr in self._patched:
                 return QBrush(Qt.blue)
-            elif status == 'ALWAYS':
+            elif status == "ALWAYS":
                 return QBrush(Qt.green)
             else:
                 return QBrush(Qt.red)
 
         elif role == QtCore.Qt.TextAlignmentRole:
             return QtCore.Qt.AlignCenter
-        
+
         elif role == QtCore.Qt.ToolTipRole:
             if index.column() == 3:
                 return "Number of unseen Basic Blocks reachable in N jumps"
             else:
                 return None
 
-    
     def sort(self, column, sort_order):
         self._data.sort(key=lambda x: x[column], reverse=sort_order)
         self.layoutChanged.emit()
@@ -64,23 +64,19 @@ class TableModel(QAbstractTableModel):
         self._last_sort = column
         self._last_sort_order = sort_order
 
-
     def headerData(self, column, orientation, role=QtCore.Qt.DisplayRole):
         if orientation == QtCore.Qt.Horizontal:
             if role == QtCore.Qt.DisplayRole:
                 return self.COLUMN_HEADERS[column]
 
-
     def rowCount(self, index):
         return len(self._data)
-
 
     def columnCount(self, index):
         if self._data:
             return len(self._data[0])
         else:
             return 0
-
 
     def setRowColor(self, index, color):
         for j in range(self.columnCount(0)):
@@ -105,7 +101,7 @@ class JumpOverview(QWidget, DockContextHandler):
         self.table.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-        # sorting 
+        # sorting
         self.table.setSortingEnabled(True)
         self.table.horizontalHeader().setSortIndicator(0, QtCore.Qt.AscendingOrder)
 
@@ -113,7 +109,7 @@ class JumpOverview(QWidget, DockContextHandler):
         self.model = TableModel(data)
         self.table.setModel(self.model)
         table_layout.addWidget(self.table)
-        
+
         layout = QVBoxLayout()
         layout.addLayout(table_layout)
         self.setLayout(layout)
@@ -122,7 +118,7 @@ class JumpOverview(QWidget, DockContextHandler):
         self.table.doubleClicked.connect(self._ui_entry_double_click)
         # init right click menu
         self.ctx_menu = QMenu()
-        self._action_patch = QAction("Invert Branch", None) 
+        self._action_patch = QAction("Invert Branch", None)
         self.ctx_menu.addAction(self._action_patch)
         self.setContextMenuPolicy(Qt.CustomContextMenu)
         self.customContextMenuRequested.connect(self._ui_table_ctx_menu_handler)
@@ -131,16 +127,14 @@ class JumpOverview(QWidget, DockContextHandler):
         self.filename = None
         self.do_sync = True
 
-
     def _ui_entry_double_click(self, index):
         self.navigate_to_addr(index)
 
-    
     def _ui_table_ctx_menu_handler(self, pos):
         action = self.ctx_menu.exec_(self.table.viewport().mapToGlobal(pos))
         if not action:
             return
-        
+
         rows = self.table.selectionModel().selectedRows()
         indices = [i.row() for i in rows]
         if len(indices) > 0 and action == self._action_patch:
@@ -153,18 +147,18 @@ class JumpOverview(QWidget, DockContextHandler):
                         self.model._patched.remove(addr)
                     self.model.data(rows[i], role=QtCore.Qt.BackgroundRole)
                 else:
-                    interaction.show_message_box("Patching Error", 
+                    interaction.show_message_box(
+                        "Patching Error",
                         "This architecture does not seem to support auto-patching yet.",
-                        icon=MessageBoxIcon.ErrorIcon)
+                        icon=MessageBoxIcon.ErrorIcon,
+                    )
 
-    
     def navigate_to_addr(self, index):
         addr = int(self.model._data[index.row()][0], 16)
         dh = DockHandler.getActiveDockHandler()
         vf = dh.getViewFrame()
         vi = vf.getCurrentViewInterface()
         vi.navigate(addr)
-
 
     def patch(self, addr):
         if self.bv is None:
@@ -174,14 +168,12 @@ class JumpOverview(QWidget, DockContextHandler):
         except:
             return False
 
-
     def notifyViewChanged(self, view_frame):
         if view_frame is None:
             pass
         else:
             self.bv = view_frame.actionContext().binaryView
             self.filename = self.bv.file.original_filename
-    
 
     @staticmethod
     def create_widget(name, parent, data=None):
@@ -194,14 +186,13 @@ class Importer(BackgroundTaskThread):
         self.view = view
         self.offset = view.start
         self.file = file_name
-        self.bv =  view
-        
+        self.bv = view
 
     def run(self):
-        with open(self.file, 'r') as f:
+        with open(self.file, "r") as f:
             data = []
             for l in f:
-                args = l.split(' ')
+                args = l.split(" ")
                 data.append(args)
                 addr = int(args[0], 16)
                 if "ALWAYS" in args[2]:
@@ -215,7 +206,7 @@ class Importer(BackgroundTaskThread):
             x[1] = x[1][10:]
             x[2] = x[2][:-6]
             x[3] = int(x[3])
-        
+
         dock_handler = DockHandler.getActiveDockHandler()
         table = dock_handler.getDockWidget("JMPscare Overview").widget()
 
@@ -223,19 +214,31 @@ class Importer(BackgroundTaskThread):
         table.table.setModel(table.model)
 
         log.log(1, "[JMPscare] Successfully imported analysis data")
-        
+
 
 def import_data(bv):
-    file_name = interaction.get_open_filename_input("Choose JMPscare analysis output file.")
+    file_name = interaction.get_open_filename_input(
+        "Choose JMPscare analysis output file."
+    )
     i = Importer(bv, file_name)
     i.run()
-    
+
 
 def openDockWidget():
     dock_handler = DockHandler.getActiveDockHandler()
-    dock_handler.addDockWidget("JMPscare Overview", JumpOverview.create_widget, Qt.RightDockWidgetArea, Qt.Vertical, True)
+    dock_handler.addDockWidget(
+        "JMPscare Overview",
+        JumpOverview.create_widget,
+        Qt.RightDockWidgetArea,
+        Qt.Vertical,
+        True,
+    )
 
 
 # view.always_branch ,  view.convert_to_nop / never_branch, view.invert_branch, save
-PluginCommand.register("JMPscare\\Import Data", "Import analysis data previously generated with the JMPscare anlysis tool", import_data)
+PluginCommand.register(
+    "JMPscare\\Import Data",
+    "Import analysis data previously generated with the JMPscare anlysis tool",
+    import_data,
+)
 openDockWidget()
