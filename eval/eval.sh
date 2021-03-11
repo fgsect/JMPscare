3f7c53d5cae8ea327960ea06834bd0d1cf49f1aa
Dominik Maier <domenukk@gmail.com>
2021-03-11 01:12:40 +0100

    eval script fix

--- eval/eval.sh
+++ eval/eval.sh
@@ -5,7 +5,7 @@ while true; do
         ../../analysis/target/release/jmpscare -n 1000 -y -a ARM ../modem_raw.img --traces ./cov >> eval.log
         cd cov
 	ls . | tail -n 1
-	ls . | tail -n 1 >> eval.log
+	ls . | tail -n 1 >> ../eval.log
         rm $(ls . | tail -n 1) || exit 0
         cd ..
 done
